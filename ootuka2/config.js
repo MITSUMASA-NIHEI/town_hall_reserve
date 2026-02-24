19ce5e5a8332495672f10435217a06872abe1eae
MITSUMASA-NIHEI <mitsumasa@xqd.biglobe.ne.jp>
2026-02-24 15:45:22 +0900

    Add files via upload

---
+++ ootuka2/config.js
@@ -0,0 +1,14 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://htrsbjadomyonuelzups.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_KQvUS5eLkDyB4dJQlhzoxw_lgNq_66F",
+
+  ORG_ID: "38a79eb3-f4c3-4365-b5f3-87aa062fa9d0",
+  ORG_SLUG: "ootuka2",
+  ORG_NAME: "大塚二区町内会",
+
+  MODE: "town",
+
+  ADMIN_PASS: "ootuka2-1234",
+  AUTH_TTL_HOURS: 8,
+  AUTH_STORAGE_KEY: "hb_admin_auth_v1"
+};
